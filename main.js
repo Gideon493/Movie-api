eb466071e5972ebe1038bce9ab018925c160c854
Gideon K <65413141+Gideon493@users.noreply.github.com>
2022-06-02 12:25:04 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-
-createApp(App).use(router).mount('#app')
